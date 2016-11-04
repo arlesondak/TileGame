9ddc10d39caa8f99cbcfe6e20f02b3654198bcde
arlesondak <arlesondak@gmail.com>
2016-11-04 10:55:42 -0700

    Finished lesson 7 - testing NetBeans git branches

--- TileGame/src/dev/codenmore/tilegame/Launcher.java
+++ TileGame/src/dev/codenmore/tilegame/Launcher.java
@@ -8,6 +8,8 @@ public class Launcher {
 	public static void main(String[] args){
 		Game game = new Game("Tile Game!", 500, 650);
 		game.start();
+                Game game2 = new Game("Test", 200, 300);
+                game2.start();
 	}
 	
 }
